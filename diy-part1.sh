fb7d112cdd56ee7f059bfb51750e05e55c611731
cnzd <foxofcn@gmail.com>
2021-03-14 20:53:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-git clone https://github.com/cnzd/luci-app-koolproxyR package/luci-app-koolproxyR
+git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
